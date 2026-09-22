fdd9ae13de20d3a9b77bb003fc8cb5dfb637f429
idasamssongmailcom <idasamsson@gmail.com>
2026-09-22 13:31:55 +0200

    Deleted some locations and added a PoliceStation-location.

--- Locations/PoliceStation.cs
+++ Locations/PoliceStation.cs
@@ -1,7 +1,7 @@
 // Example location with a submenu (the "-" lines) and an item that disappears.
 // The key goes into Player.Inventory so other locations can check for it.
 
-class Courtyard : Location
+class PoliceStation : Location
 {
     private bool _fountainSearched;
 
