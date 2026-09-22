2e1d5e199379fac5467c593dd0b7fd950c829b6c
Ironboy <ironboy@users.noreply.github.com>
2026-09-22 11:34:34 +0200

    better comments rows and cols setting start position

--- Game.cs
+++ Game.cs
@@ -25,7 +25,7 @@ class Game : Interactive
             [new RuneRoom(), new Hallway()],
             [null,           new Courtyard()],
             [null,           new Gate()],
-        ], 0 /* row */, 0 /* col */); // 0,0 = RuneRoom
+        ], 0, 0); // 0,0 = RuneRoom row = 0, col = 0
 
         world.Play();
     }
