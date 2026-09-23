39292a69beab6ab90e3c5bd5e78bc146b6eace80
Eimbiox <eimantas.fedenko04@gmail.com>
2026-09-23 13:20:03 +0200

    Fixed silvercave interactions

--- Locations/SilverCave.cs
+++ Locations/SilverCave.cs
@@ -2,7 +2,7 @@ class SilverCave : Location
 {
     private bool _speakerIsOff;
     private bool _knowOfSpeaker;
-    
+    private bool _projectorOff;
     public override string Name => "Inne i gråttan";
 
      public override string[] Description => [
@@ -14,17 +14,18 @@ class SilverCave : Location
 
     public override string[] Actions => [
        "Kolla på högtalaren",
-       "-Stäng av:TurnOffSpeaker",
-       "-Headbanga:Headbang",
-       "Gå till projektorn",
-       "-Stäng av projektorn:TurnOffProjector",
+       _speakerIsOff 
+            ? "-Högtalaren är avstängd:SpeakerAlreadyOff"
+            : "-Stäng av:TurnOffSpeaker",
+        _speakerIsOff
+            ? "-Lyssna på tystnaden:LitsenToSilence"
+            : "-Bara står där och tänk på livet:JustStandThere",
+        "Gå till projektorn",
+        _projectorOff
+            ? "-Projektorn är avstängd:ProjectorAlreadyOff"
+            : "-Stäng av projektorn:TurnOffProjector"
+       
     ];
-
-    public void Headbang()
-    {
-        Console.WriteLine("Du headbanger till den coola Sven-Ingvars-musiken...");
-        Console.ReadLine();
-    }
 
     public void TurnOffSpeaker()
     {
@@ -35,6 +36,7 @@ class SilverCave : Location
             Console.WriteLine("Du ser blodspår på högtalaren så du tar med den som bevis");
             Player.Inventory.Add("Högtalare");
             _speakerIsOff = true;
+            Menu.Close();
         }
         else
         {
@@ -42,11 +44,34 @@ class SilverCave : Location
         }
         Console.ReadLine();
     }
+    public void SpeakerAlreadyOff()
+    {
+        Console.WriteLine("Inget att se den är avstängd...");
+        Console.ReadKey();
+    }
     public void TurnOffProjector()
     {
         Console.WriteLine("Du stänger av projektorn. Turisten kankse vågar prata med dig\nDu tar en bild så du kan bevisa att spöket var fake");
+        _projectorOff = true;
         Player.Inventory.Add("Bluffbevis");
         Console.ReadKey();
+        Menu.Close();
+    }
+    public void ProjectorAlreadyOff()
+    {
+        Console.WriteLine($"Inget att se den är avstängd...");
+        Console.ReadKey();
+    }
+    public void LitsenToSilence()
+    {
+        Console.WriteLine($"Du står still och lyssnar på tystnaden\nDu får ångest så du skiter i det");
+        Console.ReadKey();
+    }
+    public void JustStandThere()
+    {
+        Console.WriteLine($"Du försöker tänka på ditt liv men ljudet stör dig...");
+        Console.ReadKey();
+        
     }
 
 }
